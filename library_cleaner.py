94aaa9f2a096363ce1e053d979f055be95204e15
s10 <68397573+s10-n@users.noreply.github.com>
2020-09-01 22:57:07 -0700

    finished a working version of the script and added comments

--- library_cleaner.py
+++ library_cleaner.py
@@ -4,40 +4,37 @@ from mutagen.id3 import ID3
 from mutagen.flac import FLAC
 from mutagen.mp4 import MP4
 from pathlib import Path
-def library_cleaner():
-    library_path = 'c:/Users/User/Projects/library_cleaner/test music'
+def library_cleaner(library_path):
+    # the below regex is used to filter out characters that Windows doesn't like in directory names
+    folder_regex = re.compile(r'\*|\\|\||<|>|/|\?|:|"')
+    # program starts by cycling through all of the artist subfolders in the main music folder
     for artist_name in os.listdir(library_path):
+        # the artist folder path is obtained by combining the main music folder path with the artist name
         artist_folder = library_path + '/' + artist_name
+        # the program then iterates through all of the albums in the artist's folder
         for album_name in os.listdir(artist_folder):
             album_folder = artist_folder + '/' + album_name
-            first_track_filename = os.listdir(album_folder)[0]
-            # if the file is an mp3
-            if Path(first_track_filename).suffix == '.mp3':
-                first_track = ID3(album_folder + '/' + first_track_filename)
-                real_album_name = first_track['TALB'].text[0]
-                real_album_name =  re.sub(r'\*|\\|\||<|>|/|\?|:|"','',real_album_name)
-                print(os.path.join(artist_folder,real_album_name))
-                os.rename(album_folder,os.path.join(artist_folder,real_album_name))
-            # if the file is a flac
-            if Path(first_track_filename).suffix == '.flac':
-                first_track = FLAC(album_folder + '/' + first_track_filename)
-#                real_album_name = first_track['TALB'].text[0]
-                real_album_name =  re.sub(r'\*|\\|\||<|>|/|\?|:|"','',first_track['album'][0])
-                print(os.path.join(artist_folder,real_album_name))
-                os.rename(album_folder,os.path.join(artist_folder,real_album_name))
-            if Path(first_track_filename).suffix == '.m4a':
+            # this determines if the first file in the folder is an audio file or not. TODO - could be cleaned up to actually iterate through items in folder
+            if os.listdir(album_folder):
+                if Path(album_folder + '/' + os.listdir(album_folder)[0]).suffix != '.flac' or '.mp3' or '.m4a':
+                    first_track_filename = os.listdir(album_folder)[-1]
+                else:
+                    first_track_filename = os.listdir(album_folder)[0]
+                # if the file is an mp3
+                if Path(first_track_filename).suffix == '.mp3':
+                    first_track = ID3(album_folder + '/' + first_track_filename)
+                    real_album_name =  re.sub(folder_regex,'',first_track['TALB'].text[0])
+                    print(f'Renamed {album_name} to {real_album_name}.')
+                    os.rename(album_folder,os.path.join(artist_folder,real_album_name))
+                # if the file is a flac
+                if Path(first_track_filename).suffix == '.flac':
+                    first_track = FLAC(album_folder + '/' + first_track_filename)
+                    real_album_name =  re.sub(folder_regex,'',first_track['album'][0])
+                    print(f'Renamed {album_name} to {real_album_name}.')
+                    os.rename(album_folder,os.path.join(artist_folder,real_album_name))
                 # if the file is an m4a
- #               print(album_folder + '/' + first_track_filename)
-  #              print(MP4(album_folder + '/' + first_track_filename)['\xa9alb'])
-                first_track = MP4(album_folder + '/' + first_track_filename)
-#                real_album_name = str(first_track['\xa9alb']).strip("[']")
-                real_album_name =  re.sub(r'\*|\\|\||<|>|/|\?|:|"','',str(first_track['\xa9alb'][0]))
-                print(os.path.join(artist_folder,real_album_name))
-                os.rename(album_folder,os.path.join(artist_folder,real_album_name))
-
-
-
-
-
-
-library_cleaner()
+                if Path(first_track_filename).suffix == '.m4a':
+                    first_track = MP4(album_folder + '/' + first_track_filename)
+                    real_album_name =  re.sub(folder_regex,'',str(first_track['\xa9alb'][0]))
+                    print(f'Renamed {album_name} to {real_album_name}.')
+                    os.rename(album_folder,os.path.join(artist_folder,real_album_name))
